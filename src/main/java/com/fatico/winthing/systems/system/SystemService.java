54ce70a95c7ada25aaa681cda5bb1045c6f2ba24
Mikołaj Siedlarek <mikolaj@siedlarek.pl>
2017-04-22 16:31:23 +0200

    Remove unused field.

--- src/main/java/com/fatico/winthing/systems/system/SystemService.java
+++ src/main/java/com/fatico/winthing/systems/system/SystemService.java
@@ -2,17 +2,16 @@ package com.fatico.winthing.systems.system;
 
 import com.fatico.winthing.windows.SystemException;
 import com.fatico.winthing.windows.jna.Advapi32;
-import com.fatico.winthing.windows.jna.User32;
 
 import com.google.common.collect.ImmutableList;
 import com.google.inject.Inject;
 import com.sun.jna.platform.win32.Kernel32;
-import com.sun.jna.platform.win32.Kernel32Util;
 import com.sun.jna.platform.win32.Shell32;
-import com.sun.jna.platform.win32.Tlhelp32;
 import com.sun.jna.platform.win32.WinDef;
 import com.sun.jna.platform.win32.WinNT;
+import com.sun.jna.platform.win32.Kernel32Util;
 import com.sun.jna.platform.win32.WinUser;
+import com.sun.jna.platform.win32.Tlhelp32;
 
 import java.util.HashMap;
 import java.util.List;
@@ -25,15 +24,13 @@ public class SystemService {
         WinNT.SE_SHUTDOWN_NAME
     );
 
-    private final User32 user32;
     private final Kernel32 kernel32;
     private final Advapi32 advapi32;
     private final Shell32 shell32;
 
     @Inject
-    public SystemService(final User32 user32, final Kernel32 kernel32, final Advapi32 advapi32,
+    public SystemService(final Kernel32 kernel32, final Advapi32 advapi32,
             final Shell32 shell32) throws SystemException {
-        this.user32 = Objects.requireNonNull(user32);
         this.kernel32 = Objects.requireNonNull(kernel32);
         this.advapi32 = Objects.requireNonNull(advapi32);
         this.shell32 = Objects.requireNonNull(shell32);
